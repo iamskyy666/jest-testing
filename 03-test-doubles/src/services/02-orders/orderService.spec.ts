6014fba35252a20aa69ca5e48bfe8ff48994c563
Soumadip <39thdecember@gmail.com>
2026-08-29 19:58:36 +0530

    Refactor ✅

--- 03-test-doubles/src/services/02-orders/orderService.spec.ts
+++ 03-test-doubles/src/services/02-orders/orderService.spec.ts
@@ -6,33 +6,48 @@ class InventoryServicesStub {
     this.inStock = value;
   }
 
-  checkStock(productId: string): boolean {
-    console.log("productId:", productId);
+  checkStock(_productId: string): boolean {
+    // console.log("productId:", productId);
     return this.inStock;
   }
 }
 
+type Transaction = { amount: number; status: string }; // refactored
+
 class FakePaymentGateway {
-  private transactions: { amount: number; status: string }[] = [];
+  private transactions: Transaction[] = [];
   processPayment(amount: number): string {
     this.transactions.push({ amount, status: "success" });
     return `Processed payment of $${amount}`;
   }
 
-  getTransactions(): { amount: number; status: string }[] {
+  getTransactions(): Transaction[] {
     return this.transactions;
   }
 }
 
 // Stubs And Fakes in action..
 describe("OrdersService", () => {
-  it("process payment and calculate bonus pts. correctly when in stock", () => {
-    const fakePaymentGateway = new FakePaymentGateway();
-    const inventoryStub = new InventoryServicesStub();
-    const orderService = new OrderService(fakePaymentGateway, inventoryStub);
+  let fakePaymentGateway: FakePaymentGateway;
+  let inventoryStub: InventoryServicesStub;
+  let orderService: OrderService;
+  let amount: number;
+  let productId: string;
 
-    const result = orderService.checkout(100, "PROD111");
-    expect(result).toBe("Processed payment of $100 - Earned 10 bonus points!");
+  beforeEach(() => {
+    console.log("Before each.. ✅");
+    fakePaymentGateway = new FakePaymentGateway();
+    inventoryStub = new InventoryServicesStub();
+    orderService = new OrderService(fakePaymentGateway, inventoryStub);
+    amount = 100;
+    productId = "PROD123";
+  });
+
+  it("process payment and calculate bonus pts. correctly when in stock", () => {
+    const result = orderService.checkout(amount, productId);
+    expect(result).toBe(
+      `Processed payment of $${amount} - Earned 10 bonus points!`,
+    );
     expect(fakePaymentGateway.getTransactions()).toEqual([
       {
         amount: 100,
@@ -42,23 +57,16 @@ describe("OrdersService", () => {
   });
 
   it("order fails when product is out of stock", () => {
-    const fakePaymentGateway = new FakePaymentGateway();
-    const inventoryStub = new InventoryServicesStub();
     inventoryStub.setInStock(false); // mimicking out-of-stock in DB.
-    const orderService = new OrderService(fakePaymentGateway, inventoryStub);
-
-    const result = orderService.checkout(100, "PROD111");
+    const result = orderService.checkout(amount, productId);
     expect(result).toBe("Order failed: Product out of stock");
     expect(fakePaymentGateway.getTransactions()).toEqual([]);
   });
 
   it("handles decimal amounts for bonus pts.", () => {
-    const fakePaymentGateway = new FakePaymentGateway();
-    const inventoryStub = new InventoryServicesStub();
-    const orderService = new OrderService(fakePaymentGateway, inventoryStub);
-
-    const result = orderService.checkout(66.99, "PROD111");
-    expect(result).toBe("Processed payment of $66.99 - Earned 6 bonus points!");
+    amount = 66.99;
+    const result = orderService.checkout(amount, productId);
+    expect(result).toBe(`Processed payment of $${amount} - Earned 6 bonus points!`);
     expect(fakePaymentGateway.getTransactions()).toEqual([
       {
         amount: 66.99,
@@ -67,3 +75,37 @@ describe("OrdersService", () => {
     ]);
   });
 });
+
+/*
+$ npm test -- orderService
+
+> 03-test-doubles@1.0.0 test
+> jest orderService
+
+  console.log
+    Before each.. ✅
+
+      at Object.<anonymous> (src/services/02-orders/C:/Users/ASUS/Desktop/jest-testing/03-test-doubles/src/services/02-orders/orderService.spec.ts:38:13)
+
+  console.log
+    Before each.. ✅
+
+      at Object.<anonymous> (src/services/02-orders/C:/Users/ASUS/Desktop/jest-testing/03-test-doubles/src/services/02-orders/orderService.spec.ts:38:13)
+
+  console.log
+    Before each.. ✅
+
+      at Object.<anonymous> (src/services/02-orders/C:/Users/ASUS/Desktop/jest-testing/03-test-doubles/src/services/02-orders/orderService.spec.ts:38:13)
+
+ PASS  src/services/02-orders/orderService.spec.ts
+  OrdersService
+    √ process payment and calculate bonus pts. correctly when in stock (53 ms)
+    √ order fails when product is out of stock (5 ms)
+    √ handles decimal amounts for bonus pts. (6 ms)
+
+Test Suites: 1 passed, 1 total
+Tests:       3 passed, 3 total
+Snapshots:   0 total
+Time:        0.729 s, estimated 1 s
+Ran all test suites matching orderService.
+*/
