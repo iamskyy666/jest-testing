93d879e82a62d06523338edfa99b0d227788a749
Soumadip <39thdecember@gmail.com>
2026-08-29 19:39:27 +0530

    Stubs And Fakes in action

--- 03-test-doubles/src/services/02-orders/orderService.spec.ts
+++ 03-test-doubles/src/services/02-orders/orderService.spec.ts
@@ -7,6 +7,7 @@ class InventoryServicesStub {
   }
 
   checkStock(productId: string): boolean {
+    console.log("productId:", productId);
     return this.inStock;
   }
 }
@@ -22,3 +23,47 @@ class FakePaymentGateway {
     return this.transactions;
   }
 }
+
+// Stubs And Fakes in action..
+describe("OrdersService", () => {
+  it("process payment and calculate bonus pts. correctly when in stock", () => {
+    const fakePaymentGateway = new FakePaymentGateway();
+    const inventoryStub = new InventoryServicesStub();
+    const orderService = new OrderService(fakePaymentGateway, inventoryStub);
+
+    const result = orderService.checkout(100, "PROD111");
+    expect(result).toBe("Processed payment of $100 - Earned 10 bonus points!");
+    expect(fakePaymentGateway.getTransactions()).toEqual([
+      {
+        amount: 100,
+        status: "success",
+      },
+    ]);
+  });
+
+  it("order fails when product is out of stock", () => {
+    const fakePaymentGateway = new FakePaymentGateway();
+    const inventoryStub = new InventoryServicesStub();
+    inventoryStub.setInStock(false); // mimicking out-of-stock in DB.
+    const orderService = new OrderService(fakePaymentGateway, inventoryStub);
+
+    const result = orderService.checkout(100, "PROD111");
+    expect(result).toBe("Order failed: Product out of stock");
+    expect(fakePaymentGateway.getTransactions()).toEqual([]);
+  });
+
+  it("handles decimal amounts for bonus pts.", () => {
+    const fakePaymentGateway = new FakePaymentGateway();
+    const inventoryStub = new InventoryServicesStub();
+    const orderService = new OrderService(fakePaymentGateway, inventoryStub);
+
+    const result = orderService.checkout(66.99, "PROD111");
+    expect(result).toBe("Processed payment of $66.99 - Earned 6 bonus points!");
+    expect(fakePaymentGateway.getTransactions()).toEqual([
+      {
+        amount: 66.99,
+        status: "success",
+      },
+    ]);
+  });
+});
